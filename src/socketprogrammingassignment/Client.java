5b80d51bb832c5aa1538a694678dceb699b8aac8
Yash Sharma <ys909@snu.edu.in>
2020-12-10 21:14:50 +0530

    updated files

--- src/socketprogrammingassignment/Client.java
+++ src/socketprogrammingassignment/Client.java
@@ -1,9 +1,8 @@
 /*
-    Socket Programming assignment client file
-    @author Yash Sharma
-    Roll no. 1810110283
-*/
-
+Socket Programming assignment client file
+@author Yash Sharma
+Roll no. 1810110283
+ */
 package socketprogrammingassignment;
 
 import java.awt.image.BufferedImage;
@@ -15,23 +14,31 @@ import javax.imageio.ImageIO;
 public class Client {
 
     private Socket socket = null;
-    private BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+    private BufferedReader bufferedReader = null;
     private BufferedReader bufferedReaderReadFromServer = null;
     private DataOutputStream dataOutputStream = null;
     private InputStream inputStream = null;
 
     public Client(String address, int port) {
         try {
+// create a new socket object with given address and port
             socket = new Socket(address, port);
+            System.out.println("Connected"); // at this point client is connected to server via the mentioned port
+// take input from client/user through terminal
+            bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+// send output to socket
             dataOutputStream = new DataOutputStream(socket.getOutputStream());
+// read from server
             bufferedReaderReadFromServer = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+// read from input Stream
             inputStream = socket.getInputStream();
         } catch (IOException e) {
             System.out.println(e);
         }
-
+// take inputs from client
         try {
-            System.out.println("Enter matrix");
+// take input for adjacency matrix
+            System.out.println("Enter a space seperated adjancency matrix (5x5):");
             int matrixLength = 5;
             dataOutputStream.writeInt(matrixLength);
             for (int i = 0; i < matrixLength; i++) {
@@ -41,25 +48,33 @@ public class Client {
                     dataOutputStream.writeInt(Integer.parseInt(arr1));
                 }
             }
-            System.out.println("Enter n:");
+// take input for path length
+            System.out.println("Enter the length of the path (n):");
             dataOutputStream.writeInt(Integer.parseInt(bufferedReader.readLine().trim()));
-            System.out.println("Enter 1st node");
+// take input for node1
+            System.out.println("Enter node 1 (upper cased letter, e.g., 'A', 'B' ... 'E'):");
             dataOutputStream.writeChar(bufferedReader.readLine().trim().charAt(0));
-            System.out.println("Enter 2nd node");
+// take input for node2
+            System.out.println("Enter node 2 (upper cased letter,e.g., 'A', 'B' ... 'E'):");
             dataOutputStream.writeChar(bufferedReader.readLine().trim().charAt(0));
+// console log the value (YES/NO)
             System.out.println(bufferedReaderReadFromServer.readLine());
-
+// receive the image from server via input stream
             byte[] sizeAr = new byte[4];
             inputStream.read(sizeAr);
             int size = ByteBuffer.wrap(sizeAr).asIntBuffer().get();
             byte[] imageAr = new byte[size];
             inputStream.read(imageAr);
             BufferedImage image = ImageIO.read(new ByteArrayInputStream(imageAr));
-            ImageIO.write(image, "png", new File("image" + System.currentTimeMillis() + ".png"));
+// console logs for extra confirmation
+            System.out.println("Received an image of dimensions" + image.getHeight() + "x" + image.getWidth() + ": at timeStamp: " + System.currentTimeMillis() + " for confirmation.");
+            System.out.println("Image name: " + "GraphImageFrame" + System.currentTimeMillis() + ".png");
+// wites the image
+            ImageIO.write(image, "png", new File("GraphImageFrame" + System.currentTimeMillis() + ".png"));
         } catch (IOException e) {
             System.out.println(e);
         }
-
+// close all the connections
         try {
             bufferedReader.close();
             dataOutputStream.close();
@@ -70,6 +85,7 @@ public class Client {
     }
 
     public static void main(String[] args) {
-        Client client = new Client("localhost", 6789);
+// create a client class object
+        Client client = new Client("127.0.0.1", 9999);
     }
 }
--- src/socketprogrammingassignment/Server.java
+++ src/socketprogrammingassignment/Server.java
@@ -1,9 +1,8 @@
 /*
-    Socket Programming assignment server file
-    @author Yash Sharma
-    Roll no. 1810110283
-*/
-
+Socket Programming assignment server file
+@author Yash Sharma
+Roll no. 1810110283
+ */
 package socketprogrammingassignment;
 
 import java.awt.BasicStroke;
@@ -19,6 +18,7 @@ import java.net.*;
 import java.io.*;
 import java.nio.ByteBuffer;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import javax.imageio.ImageIO;
@@ -35,15 +35,24 @@ public final class Server {
     private PrintStream ps = null;
     private OutputStream outputStream = null;
 
-    public Server(int port) {     
+    public Server(int port) {
         try {
             serverSocket = new ServerSocket(port);
-            socket = serverSocket.accept(); 
+            System.out.println("Server Started at port " + port + ".\nWaiting for client...");
+            socket = serverSocket.accept(); // accepts the client
+            System.out.println("Client accepted succesfully!");
             dataInputStream = new DataInputStream(new BufferedInputStream(socket.getInputStream()));
+// for writing to client
             ps = new PrintStream(socket.getOutputStream());
+// outputStream to send the image
             outputStream = socket.getOutputStream();
-
+        } catch (IOException e) {
+            System.out.println(e);
+        }
+        try {
+// receive the length of adjancency list i.e., 5 here
             int length = dataInputStream.readInt();
+// receive values entered by client and make a adjancy list out of the values
             adjList = new ArrayList[length];
             adjList2 = new ArrayList[length];
             for (int i = 0; i < length; i++) {
@@ -59,24 +68,32 @@ public final class Server {
                     }
                 }
             }
-
+// reading the other 3 values
             int pathLength = dataInputStream.readInt();
             Character node1 = dataInputStream.readChar();
             Character node2 = dataInputStream.readChar();
+// map letters with the adjacency list
             HashMap<Character, Integer> map = new HashMap<>();
             map.put('A', 0);
             map.put('B', 1);
             map.put('C', 2);
             map.put('D', 3);
             map.put('E', 4);
-            getAllPaths(map.get(node1), map.get(node2), length, pathLength);
-
+// function which runs the algorithm
+// return the String to client {YES/NO}
             if (answerPathList.size() > 0) {
                 ps.println("Yes, there exists a path of length " + pathLength + " from node " + node1 + " to node " + node2 + ".");
             } else {
                 ps.println("No, there is no path of length " + pathLength + " from node " + node1 + " to node " + node2 + ".");
             }
-            // make your own grpah frame here
+// open the JavaFrame for cutom painting
+            GraphFrame gf = new GraphFrame(adjList2, outputStream);
+        } catch (IOException e) {
+            System.out.println(e);
+        }
+        System.out.println("Closing connection");
+// close all the connections
+        try {
             socket.close();
             dataInputStream.close();
             ps.close();
@@ -84,6 +101,7 @@ public final class Server {
             System.out.println(e);
         }
     }
+// Algorithm
 
     public void getAllPaths(int node1, int node2, int length, int pathLength) {
         boolean[] visitedArray = new boolean[length];
@@ -91,35 +109,36 @@ public final class Server {
         pathList.add(node1);
         getAllPathsUtil(node1, node2, visitedArray, pathList, pathLength);
     }
+// Algorithm util function
 
     private void getAllPathsUtil(Integer u, Integer d, boolean[] isVisited, List<Integer> localPathList, int pathLength) {
-        if (u.equals(d) && localPathList.size()-1 == pathLength) {
+        if (u.equals(d) && localPathList.size() - 1 == pathLength) {
             answerPathList.add(localPathList);
             return;
         }
         isVisited[u] = true;
-
-        adjList[u].stream().filter((i) -> (!isVisited[i])).map((i) -> {
-            localPathList.add(i);
-            return i;
-        }).map((i) -> {
-            getAllPathsUtil(i, d, isVisited, localPathList, pathLength);
-            return i;
-        }).forEachOrdered((i) -> {
-            localPathList.remove(i);
-        });
+        for (Integer i : adjList[u]) {
+            if (!isVisited[i]) {
+                localPathList.add(i);
+                getAllPathsUtil(i, d, isVisited, localPathList, pathLength);
+                localPathList.remove(i);
+            }
+        }
         isVisited[u] = false;
     }
 
     public static void main(String[] args) {
-        Server server = new Server(6789);
+// create a server object
+        Server server = new Server(9999);
     }
 }
 
 class GraphFrame extends JFrame {
+// receive the adjacencyList and outputStream from server class in constructor
 
     public GraphFrame(ArrayList<Integer>[] adjList, OutputStream outputStream) {
         super("GraphFrame");
+// create a plain canvas for painting graph
         Canvas c = new Canvas() {
             @Override
             public void paint(Graphics g) {
@@ -127,24 +146,27 @@ class GraphFrame extends JFrame {
                 for (int i = 0; i < 5; i++) {
                     centers[i] = new ArrayList<>();
                 }
+// center top
                 centers[0].add(275);
                 centers[0].add(75);
+// left top
                 centers[1].add(75);
                 centers[1].add(225);
+// right top
                 centers[2].add(475);
                 centers[2].add(225);
+// left bottom
                 centers[3].add(125);
                 centers[3].add(425);
+// right bottom
                 centers[4].add(425);
                 centers[4].add(425);
                 Graphics2D g2 = (Graphics2D) g;
                 g2.setColor(Color.decode("#0a1430"));
                 g2.fillRect(0, 0, 550, 550);
                 g2.setStroke(new BasicStroke(3));
-
                 g2.setColor(Color.decode("#357aa6"));
                 boolean[] ar = {false, false, false, false, false};
-
                 for (int i = 0; i < adjList.length; i++) {
                     for (int j = 0; j < adjList.length; j++) {
                         if (adjList[i].get(j) == 1) {
@@ -166,43 +188,43 @@ class GraphFrame extends JFrame {
                 }
                 g2.setStroke(new BasicStroke(2));
                 g2.setColor(Color.WHITE);
-
+// A-B
                 Point sw = new Point(250, 100);
                 Point ne = new Point(100, 200);
                 drawLogicLine(g2, 0, 1, sw, ne, adjList);
-
+// B-D
                 sw = new Point(75, 260);
                 ne = new Point(100, 400);
                 drawLogicLine(g2, 1, 3, sw, ne, adjList);
-
+// D-E
                 sw = new Point(160, 425);
                 ne = new Point(390, 425);
                 drawLogicLine(g2, 3, 4, sw, ne, adjList);
-
+// C-E
                 sw = new Point(475, 260);
                 ne = new Point(440, 395);
                 drawLogicLine(g2, 2, 4, sw, ne, adjList);
-
+// B-C
                 sw = new Point(110, 225);
                 ne = new Point(440, 225);
                 drawLogicLine(g2, 1, 2, sw, ne, adjList);
-
+// A-C
                 sw = new Point(300, 100);
                 ne = new Point(450, 200);
                 drawLogicLine(g2, 0, 2, sw, ne, adjList);
-
+// A-D
                 sw = new Point(265, 120);
                 ne = new Point(130, 390);
                 drawLogicLine(g2, 0, 3, sw, ne, adjList);
-
+// A-E
                 sw = new Point(285, 120);
                 ne = new Point(420, 390);
                 drawLogicLine(g2, 0, 4, sw, ne, adjList);
-
+// B-E
                 sw = new Point(100, 250);
                 ne = new Point(400, 400);
                 drawLogicLine(g2, 1, 4, sw, ne, adjList);
-
+// D-C
                 sw = new Point(150, 400);
                 ne = new Point(450, 250);
                 drawLogicLine(g2, 3, 2, sw, ne, adjList);
@@ -210,25 +232,37 @@ class GraphFrame extends JFrame {
         };
         add(c);
         setSize(550, 550);
+        c.setBackground(Color.GRAY);
+// set visibility to false as we don't need to see the frame here
+// toggle it to true if you want to see the JFrame as well
         setVisible(false);
+// capture image function
         clickImage(c, outputStream);
     }
+// draw arrows based on graph
 
     private void drawArrowHead(Graphics2D g2, Point tip, Point tail) {
         double phi = Math.toRadians(40);
+// set the width of arrowhead
         double wid = 12;
         g2.setPaint(Color.WHITE);
+// set the value for dy and dx
         double dy = tip.y - tail.y;
         double dx = tip.x - tail.x;
+// angle substended by dy and dx called theta
         double theta = Math.atan2(dy, dx);
         double x, y, rho = theta + phi;
+// draw the arrows by looping through
         for (int j = 0; j < 2; j++) {
             x = tip.x - wid * Math.cos(rho);
             y = tip.y - wid * Math.sin(rho);
+// actual code which draws the arrows
             g2.draw(new Line2D.Double(tip.x, tip.y, x, y));
+// finally change the rho value
             rho = theta - phi;
         }
     }
+// draw arrows and edges based on the graph
 
     private void drawLogicLine(Graphics2D g2, int row, int col, Point sw, Point ne, ArrayList<Integer>[] adjList) {
         if (adjList[row].get(col) == 1 && adjList[col].get(row) == 1) {
@@ -241,6 +275,7 @@ class GraphFrame extends JFrame {
             drawLineWithArrows(sw, ne, g2, false, false);
         }
     }
+// draw arrows util function
 
     private void drawLineWithArrows(Point sw, Point ne, Graphics2D g2, boolean smallSide, boolean largeSide) {
         if (smallSide || largeSide) {
@@ -253,6 +288,7 @@ class GraphFrame extends JFrame {
             drawArrowHead(g2, ne, sw);
         }
     }
+// click the image of Canvas and send it to receiver using the outputStream
 
     private void clickImage(Canvas yourComponent, OutputStream outputStream) {
         try {
@@ -264,10 +300,8 @@ class GraphFrame extends JFrame {
             outputStream.write(size);
             outputStream.write(byteArrayOutputStream.toByteArray());
             outputStream.flush();
-
         } catch (IOException e) {
             System.out.println(e);
         }
     }
-
 }
